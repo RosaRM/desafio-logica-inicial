a6e6498d605b093299b1c676d1192ded5b2773e7
RosaRM <143812403+RosaRM@users.noreply.github.com>
2024-12-15 22:47:33 -0300

    meu commit

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("Hello mundo")
+let nome = prompt("Digite seu nome: ");
+let XP = prompt('Quanto XP possui: ');
+let nivel 
+
+
+if (XP < 1000) {
+    nivel = "Ferro";
+} else if (XP >= 1001 && XP <= 2000) {
+    nivel = "Bronze";
+} else if (XP >= 2001 && XP <= 5000) {
+    nivel = "Prata";
+} else if (XP >= 5001 && XP <= 7000) {
+    nivel = "Ouro";
+} else if (XP >= 7001 && XP <= 8000) {
+    nivel = "Platina";
+} else if (XP >= 8001 && XP <= 9000) {
+    nivel = "Ascendente";
+} else if (XP >= 9001 && XP <= 10000) {
+    nivel = "Imortal";
+} else if (XP > 10000) {
+    nivel = "Radiante";
+}
+
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
